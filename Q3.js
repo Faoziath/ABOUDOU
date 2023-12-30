c0b48f078bddaee531419c1dffaed31473e7badf
Faoziath <151927470+Faoziath@users.noreply.github.com>
2023-12-30 15:57:41 +0100

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,15 @@
+let mot=String (prompt("Entrer un mot"))
+function palindrome (mot)
+if (mot==mot.split('').reverse (). join('')) {
+    return mot + "Is a palindrome"
+}
+else{
+    return mot + "Is not a palindrome"
+}
+
+console.log(madam)
+
+
+
+
+
---
+++ ex2.js
@@ -0,0 +1,6 @@
+let nom=String (prompt("Entrer votre nom"));
+let prenom=String (prompt("Entrer votre prenom"));
+let anne=Number (prompt("Entrer votre année de naissance"));
+let an=1600
+anne=an-anne
+alert( "Hello nom, prénoms, vous avez age ans" +  anne )
